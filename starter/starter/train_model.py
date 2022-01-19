900e6a6c79d251b8cdecf25d87671f456f7fbe98
Björn M <bjornmauthe@gmx.net>
2022-01-19 13:54:05 +0100

    added dvc config, model tests

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -3,12 +3,26 @@
 from sklearn.model_selection import train_test_split
 from ml.model import train_model
 from ml.data import process_data
+import pandas as pd
+import logging
+import pickle
+import os
 
+# Logging
+logging.basicConfig(
+    filename='./results.log',
+    level=logging.INFO,
+    filemode='w',
+    format='%(name)s - %(levelname)s - %(message)s')
 
-# Add code to load in the data.
+# Load in the cleaned data
+data_path = os.path.join(os.getcwd(), 'data', 'cleaned_census.csv')
+data = pd.read_csv(data_path)
+logging.info('SUCCESS: Loaded cleaned census.csv data')
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
+logging.info('SUCCESS: Split data in training and test data')
 
 cat_features = [
     "workclass",
@@ -24,6 +38,18 @@ X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
-# Proces the test data with the process_data function.
+X_test, y_test, _, _ = process_data(
+    test, categorical_features=cat_features, label="salary", training=False,
+    encoder=encoder, lb=lb
+)
+logging.info('SUCCESS: Preprocessing data')
 
 # Train and save a model.
+model = train_model(X_train, y_train)
+logging.info('SUCCESS: Training model')
+
+pkl_model = 'trained_model.pkl'
+with open(pkl_model, 'wb') as f:
+    pickle.dump(model, f)
+
+logging.info('SUCCESS: Saving model')
---
+++ starter/tests/test_model.py
@@ -0,0 +1,32 @@
+from ..starter.ml.model import train_model, compute_model_metrics, inference
+from sklearn.ensemble import RandomForestClassifier
+import pytest
+import numpy as np
+
+
+@pytest.fixture(scope="session")
+def data():
+    X = np.array([[1.0, 2.0, 1.0], [3.0, 4.0, 3.0]])
+    y = np.array([[1, 0]])
+    return (X, y)
+
+
+def test_model_type(data):
+    clf = train_model(data[0], data[1])
+    assert type(clf) == RandomForestClassifier
+
+
+def test_model_metrics():
+    y_true = [0, 1, 0, 0, 1, 1]
+    y_pred = [0, 1, 0, 1, 1, 1]
+    pr, recall, fbeta = compute_model_metrics(y_true, y_pred)
+
+    assert pr == 0.75
+    assert recall == 1
+    assert fbeta == 0.8571
+
+
+def test_inference(data):
+    clf = train_model(data[0], data[1])
+    test_X = np.array([[1.0, 1.0, 1.0]])
+    assert inference(clf, test_X) == np.array([1])
